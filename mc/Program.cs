56363cc7c55d5ef3b883dd92f4bc6ef799b72d77
Chermaley <slimix23@gmail.com>
2022-10-26 14:13:37 +0400

    move pretty print to SyntaxNode

--- mc/Program.cs
+++ mc/Program.cs
@@ -36,7 +36,7 @@ namespace Compiller
                 if (showTree)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    PrettyPrint(syntaxTree.Root);
+                    syntaxTree.Root.WriteTo(Console.Out);
                     Console.ResetColor();
 
                 }
@@ -76,30 +76,6 @@ namespace Compiller
                     Console.WriteLine(result.Value);
                 }
             }
-        }
-
-        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
-        {
-            var marker = isLast ? "└──" : "├──";
-            
-            Console.Write(indent);
-            Console.Write(marker);
-            Console.Write(node.Kind);
-
-            if (node is SyntaxToken t && t.Value != null)
-            {
-                Console.Write(" ");
-                Console.Write(t.Value);
-            }
-            
-            Console.WriteLine();
-            
-            indent += isLast ? "   " : "|   ";
-
-            var lastChild = node.GetChildren().LastOrDefault();
-            
-            foreach (var child in node.GetChildren())
-                PrettyPrint(child, indent, child == lastChild);
         }
     }
 }
